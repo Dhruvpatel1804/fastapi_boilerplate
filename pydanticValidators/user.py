cce23995da851e65417fb724c9c904928f7de65f
platform <platform@getforceai.com>
2026-03-18 11:28:53 +0530

    added notes

--- pydanticValidators/user.py
+++ pydanticValidators/user.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel, EmailStr, Field
 
+# NOTE: add request body validators for APIs
 class UserSignupFields(BaseModel):
     email: EmailStr
     password: str = Field(..., min_length=8, max_length=100)
